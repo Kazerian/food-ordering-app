b019d4afbac42000f5619d36918c4dc45c2715f8
radhik10 <adhikari.riya@optum.com>
2021-05-24 01:35:03 +0530

    adding coontext code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import CartProvider from "./store/CartProvider";
 
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
@@ -7,7 +8,7 @@ import Meals from "./components/Meals/Meals";
 function App() {
   const [cartVisible, setCartVisible] = useState(false);
   return (
-    <>
+    <CartProvider>
       <Header onCartClick={() => setCartVisible(true)} />
       {cartVisible && (
         <Cart
@@ -19,7 +20,7 @@ function App() {
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
